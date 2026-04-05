a9164ee655026646e4cce7e2d7281d95af588f56
gabrielriviere999-commits <gabriel.riviere999@gmail.com>
2026-04-05 15:05:26 +0400

    Update make_search_index.py

--- make_search_index.py
+++ make_search_index.py
@@ -19,7 +19,7 @@ docs = []
 
 for root, dirs, files in os.walk(folder):
     # Retirer les dossiers inutiles de la liste parcourue
-    dirs[:] = [d for d in dirs if d not in {".git", ".github", "__pycache__", "thumbnails"}]
+    dirs[:] = [d for d in dirs if d not in {".git", ".github", "__pycache__"}]
 
     for f in files:
         # Ignorer certains fichiers générés
