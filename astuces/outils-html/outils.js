7f54795da9843210b97c41284028ea253ac2c0b5
gabrielriviere999-commits <gabriel.riviere999@gmail.com>
2026-07-27 10:28:25 +0400

    Add files via upload

--- astuces/outils-html/outils.js
+++ astuces/outils-html/outils.js
@@ -51,7 +51,7 @@ function closePopup() {
     var d = document.querySelector('.popup-overlay');
     if (!d) return;
     var popupContent = d.querySelector('.popup-content');
-    // Remettre les enfants dans leur conteneur d’origine
+    // Remettre les enfants dans leur conteneur d'origine
     while (popupContent.firstChild) {
         popupOrigin.appendChild(popupContent.firstChild);
     }
