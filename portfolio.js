96bfcc886085f083fa2dab2d37d5ebb65eaa5b4e
gabrielriviere999-commits <gabriel.riviere999@gmail.com>
2026-07-28 13:12:59 +0400

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -377,3 +377,45 @@ window.popupMenuHTML =
     '<li><a href="arbo.html">Arborescence</a></li>' +
     '<li><a href="https://github.com/gabrielriviere999-commits/portfolio">Dépôt GitHub</a></li>' +
     '</ul>';
+
+function openSearchPopup() {
+  var container = document.getElementById("popupSearchContent");
+  openPopupMini(container);
+}
+function doSearch() {
+  var q = document.getElementById("search").value.toLowerCase();
+  var out = "<ul>";
+  var found = 0;
+  for (var i=0; i<docs.length; i++) {
+    if (docs[i].content.toLowerCase().indexOf(q) !== -1) {
+      out += "<li>"
+          + "<a href='" + docs[i].url + "' target='_self'>" + docs[i].title + "</a>"
+          + " <a href='" + docs[i].url + "' download>[↓]</a>"
+          + "</li>";
+      found++;
+    }
+  }
+  out += "</ul>";
+  // Affichage du compteur
+  document.getElementById("count").textContent =
+      found > 0 ? found + " résultat(s)" : "Aucun résultat";
+  // Affichage des résultats
+  document.getElementById("results").innerHTML =
+      found > 0 ? out : "";
+  // Sauvegarde
+  sessionStorage.setItem("lastQuery", q);
+  sessionStorage.setItem("lastResults", out);
+  sessionStorage.setItem("lastCount", found);
+}
+// Au chargement de la page, restaurer
+window.onload = function() {
+  if (sessionStorage.getItem("lastQuery")) {
+    document.getElementById("search").value = sessionStorage.getItem("lastQuery");
+    document.getElementById("results").innerHTML = sessionStorage.getItem("lastResults");
+  }
+  if (sessionStorage.getItem("lastCount")) {
+    var c = sessionStorage.getItem("lastCount");
+    document.getElementById("count").textContent =
+        c > 0 ? c + " résultat(s)" : "Aucun résultat";
+  }
+};
