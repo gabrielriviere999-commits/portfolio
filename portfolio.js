68e0d26e79b9a7ab055cdc1ecbc930827afef78e
gabrielriviere999-commits <gabriel.riviere999@gmail.com>
2026-09-19 17:52:13 +0400

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -312,7 +312,7 @@ window.popupMenuHTML =
     '<ul>' +
     '<li><a href="../outils/search.html" style="color:#f44;">Rechercher</a></li>' +
     '<li><a href="../outils/arbo.html" style="color:#f44;">Arborescence</a></li>' +
-    '<ul><li><a href="../ocr/arbo.html" style="color:#f44;" download>Arborescence ocr</a></li></ul>' +
+    '<ul><li><a href="../ocr/arbo.html" style="color:#f44;">Arborescence ocr</a></li></ul>' +
     '<li><a href="https://github.com/gabrielriviere999-commits/outils" style="color:#f44;">Dépôt GitHub</a></li>' +
     '<li><a href="https://codeload.github.com/gabrielriviere999-commits/outils/zip/refs/heads/main" style="color:#f44;" download>Télécharger dépôt</a></li>' +
     '<ul><li><a href="https://codeload.github.com/gabrielriviere999-commits/ocr/zip/refs/heads/main" style="color:#f44;" download>Télécharger dépôt ocr</a></li></ul>' +
